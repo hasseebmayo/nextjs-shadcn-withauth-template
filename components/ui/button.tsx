2fb9595dee6526b35f1305230e0b51e2d1b92453
Haseeb Ahmad <hasseebmayo@gmail.com>
2024-08-22 15:53:49 +0500

    new loader added

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,6 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import LoaderIos from '../loader';
 
 const buttonVariants = cva(
  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -80,10 +81,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     {...props}
    >
     <>
-     {loading && (
-      <Loader2 className={cn('h-4 w-4 animate-spin', children && 'mr-2')} />
-     )}
-     {children}
+     {loading ? <LoaderIos className={cn('h-4 w-4 animate-spin')} /> : children}
     </>
    </button>
   );
