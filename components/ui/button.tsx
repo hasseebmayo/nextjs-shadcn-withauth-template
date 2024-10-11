b0ef0ab3431c14f8b1cfb72b8507d2b5948439f4
hasseebmayo <hasseebmayo@gmail.com>
2024-10-12 02:03:13 +0500

    button loader added

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,11 +3,10 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
-import { Loader2 } from 'lucide-react';
-import LoaderIos from '../loader';
+import { Loader } from 'lucide-react';
 
 const buttonVariants = cva(
- 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+ 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 relative',
  {
   variants: {
    variant: {
@@ -58,9 +57,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
          children: (
           <>
            {loading && (
-            <Loader2
-             className={cn('h-4 w-4 animate-spin', children && 'mr-2')}
-            />
+            <Loader className="mr-2 size-4 animate-spin" />
+            // <Loader2
+            //  className={cn('h-4 w-4 animate-spin', children && 'mr-2')}
+            // />
            )}
            {child.props.children}
           </>
@@ -81,7 +81,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     {...props}
    >
     <>
-     {loading ? <LoaderIos className={cn('h-4 w-4 animate-spin')} /> : children}
+     {loading && (
+      <Loader className={cn('absolute inset-0 m-auto h-4 w-4 animate-spin')} />
+     )}
+     {loading ? <span className="opacity-0">{children}</span> : <>{children}</>}
     </>
    </button>
   );
